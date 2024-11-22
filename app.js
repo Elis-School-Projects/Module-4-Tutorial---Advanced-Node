f63ae6f085af564942757935e0d91a6eccbea83b
Eli Walters <ewalters37@ivytech.edu>
2024-11-22 16:29:23 -0500

    Update files for module 5

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-// NPM Import
+// Package Imports
 const express = require("express");
+const morgan = require("morgan");
 
 // Express App
 const app = express();
+
+// Register view engine
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.")
 });
 
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {title: "Blog Number 1", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+        {title: "Best Blog Number 2", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+        {title: "Semi Okay Blog Number 3", snippet: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+    ]
+
+    res.render('index', { title: "Home", blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: "About" });
 });
 
-app.get('/about-us', (req, res) => res.redirect('/about'));
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "Create Blog" });
+});
 
 app.use((req, res) => {
-    res.sendStatus(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: "404" });
 });
